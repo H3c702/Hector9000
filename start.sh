9c6ca9b655a6867c5d77ae202a2ef50417181d84
DevTown <andre@dev-town.de>
2019-12-11 19:19:49 +0100

    fix old start for new

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ echo"\_| |_/\____/  \____/  \_/   \___/ \_| \_| \____/  \___/  \___/  \___/"
 echo"-----------------------------------------------------------------------"
 
 
-tmux new-session -d -s hector 'python3 src/HardwareRunner.py'
+tmux new-session -d -s hector 'python3 src/HectorServer.py'
 tmux new-window -t  hector:1 'python3 src/NeoPixel.py'
 python3 src/main.py
