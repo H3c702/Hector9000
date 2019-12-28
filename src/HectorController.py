1ce91027d5443bf2aab4c76f77c9099cb274d17d
Ben <ben@temme-home.de>
2019-12-28 11:37:12 +0100

    Removed HectorRemote and second process with server. Needs testing

--- src/HectorController.py
+++ src/HectorController.py
@@ -1,4 +1,4 @@
-from HectorRemote import HectorRemote as Hector
+import HectorServer as Hector
 
 import json
 import conf.drinks as drinks
@@ -7,6 +7,10 @@ import paho.mqtt.client as mqtt
 import traceback
 
 
+def log(obj):
+    print("Controller: " + str(obj))
+    
+    
 # settings
 class HectorController:
     MQTTServer = "localhost"
@@ -23,8 +27,7 @@ class HectorController:
         return topic + "/progress"
 
     def __init__(self):
-        self.hector = Hector()
-        print("init")
+        log("init")
 
     def available_drinks_as_JSON(self):
         datalist = []
@@ -41,15 +44,15 @@ class HectorController:
         inglist = [{"name": drinks.ingredients[step[1]][0], "ammount": step[2]} for step in drink["recipe"] if
                    step[0] == "ingr"]
         data = {"id": id, "name": drink["name"], "ingredients": inglist}
-        print(data)
+        log(data)
         return json.dumps(data)
 
     def on_connect(self, client, userdata, flags, rc):
-        print("Connected with result code " + str(rc))
+        log("Connected with result code " + str(rc))
         client.subscribe(self.TopicPrefix + "#")
 
     def on_log(self, client, userdata, level, buf):
-        pass  # print("LOG " + str(level) + ": " + str(userdata) + " -- " + str(buf))
+        pass  # log("LOG " + str(level) + ": " + str(userdata) + " -- " + str(buf))
 
     def _do_get_drinks(self, msg):
         self.client.publish(self.get_returnTopic(msg.topic), self.available_drinks_as_JSON())
@@ -72,15 +75,13 @@ class HectorController:
             progress = progress + steps
             if step[0] == "ingr":
                 pump = drinks.available_ingredients.index(step[1])
-                self.hector.valve_dose(index=int(pump), amount=int(step[2]))
-
+                Hector.do_valve_dose(index=int(pump), amount=int(step[2]))
                 self.client.publish(self.get_progressTopic(msg.topic), progress)
-
         self.client.publish(self.get_progressTopic(msg.topic), 100)
         self.client.publish(self.get_progressTopic(msg.topic), "end")
 
     def on_message(self, client, userdata, msg):
-        print("on_message: topic " + str(msg.topic) + ", msg: " + str(msg.payload))
+        log("on_message: topic " + str(msg.topic) + ", msg: " + str(msg.payload))
         try:
             global currentTopic
             currentTopic = msg.topic
@@ -103,46 +104,43 @@ class HectorController:
                 # Setzt die Ing in der DB
                 pass
             elif currentTopic == self.TopicPrefix + "light_on":
-                self.hector.light_on()
+                Hector.do_light_on()
                 pass
             elif currentTopic == self.TopicPrefix + "light_off":
-                self.hector.light_off()
+                Hector.do_light_off()
                 pass
 
             # high-level
             elif currentTopic == self.TopicPrefix + "ring":
-                self.hector.ping()
+                Hector.do_ping(2, 1)
                 pass
             elif currentTopic == self.TopicPrefix + "doseDrink":
                 self._do_dose_drink(msg)
                 pass
             elif currentTopic == self.TopicPrefix + "cleanMe":
                 # ToDo: Develop proper methode in Server
-                self.hector.all_valve_open()
-                self.hector.cleanAndExit()
+                for i in range(12):
+                    Hector.clean(1)
                 pass
                 # clean(msg)
             elif currentTopic == self.TopicPrefix + "dryMe":
                 pass
             elif currentTopic == self.TopicPrefix + "openAllValves":
-                self.hector.all_valve_open()
+                Hector.all_valve_open()
                 pass
             elif currentTopic == self.TopicPrefix + "closeAllValves":
-                self.hector.all_valve_close()
+                Hector.all_valve_close()
                 pass
-            # unknown
             else:
-                print("unknown topic: " + currentTopic + ", msg " + str(msg.payload))
+                log("unknown topic: " + currentTopic + ", msg " + str(msg.payload))
 
-            print("done with msg " + currentTopic + " / " + str(msg.payload))
+            log("handled message " + currentTopic + " / " + str(msg.payload))
 
         except Exception as e:
-            print("Error! " + str(e))
-            print(traceback.format_exc())
+            log("Error! " + str(e))
+            log(traceback.format_exc())
 
     def connect(self):
-        print("HECTORCONTROLLER")
-        # main program
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.on_log = self.on_log
--- src/HectorServer.py
+++ src/HectorServer.py
@@ -12,242 +12,152 @@ import traceback
 import paho.mqtt.client as mqtt
 
 from conf.HectorConfig import config
+
 from HectorHardware import HectorHardware as Hector
-#from HectorSimulator import HectorSimulator as Hector
 
-
-# settings
-
-MQTTServer = "localhost"
-TopicPrefix = "Hector9000/Main/"
-
-# global vars
+# from HectorSimulator import HectorSimulator as Hector
 
 hector = Hector(config)
-currentTopic = ""
 
 
-## functions
-
-# MQTT callbacks
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe(TopicPrefix + "+")
-
-
-def on_callback(name, value):
-    global currentTopic
-    print("=> %s: %d" % (name, value))
-    client.publish(currentTopic + "/progress", value)
-    client.loop()
-
-
-def on_log(client, userdata, level, buf):
-    print("LOG " + str(level) + ": " + str(userdata) + " -- " + str(buf))
+def log(message):
+    print("HARDWARELOG: " + message)
 
 
 # low-level functions
 
-def do_get_config(msg):
-    print("get configuration")
-    ret = hector.getConfig()
-    client.publish(TopicPrefix + "/return", str(ret))
+
+def do_get_config():
+    log("get configuration")
+    return  hector.getConfig()
 
 
-def do_light_on(msg):
-    print("turn on light")
+def do_light_on():
+    log("turn on light")
     hector.light_on()
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_light_off(msg):
-    print("turn off light")
+def do_light_off():
+    log("turn off light")
     hector.light_off()
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_arm_out(msg):
-    print("drive arm out")
+def do_arm_out():
+    log("drive arm out")
     hector.arm_out(on_callback)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_arm_in(msg):
-    print("drive arm in")
+def do_arm_in():
+    log("drive arm in")
     hector.arm_in(on_callback)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_arm_isInOutPos(msg):
-    print("check if arm is in OUT position")
-    ret = "1" if hector.arm_isInOutPos else "0"
-    client.publish(currentTopic + "/return", ret)
+def do_arm_isInOutPos():
+    log("check if arm is in OUT position")
+    return "1" if hector.arm_isInOutPos else "0"
 
 
-def do_scale_readout(msg):
-    print("query scale readout")
-    ret = hector.scale_readout()
-    client.publish(currentTopic + "/return", str(ret))
+def do_scale_readout():
+    log("query scale readout")
+    return hector.scale_readout()
 
 
-def do_scale_tare(msg):
-    print("set scale tare")
+def do_scale_tare():
+    log("set scale tare")
     hector.scale_tare()
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_pump_start(msg):
-    print("start pump")
+def do_pump_start():
+    log("start pump")
     hector.pump_start()
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_pump_stop(msg):
-    print("stop pump")
+def do_pump_stop():
+    log("stop pump")
     hector.pump_stop()
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_all_valve_open(msg):
+def do_all_valve_open():
     hector.light_on()
     time.sleep(1)
     hector.arm_in()
     hector.pump_stop()
     for vnum in range(12):
-        print("Ventil %d wird geöffnet" % (vnum,))
+        log("Ventil %d wird geöffnet" % (vnum,))
         time.sleep(1)
         hector.valve_open(vnum)
     hector.light_off()
 
 
-def do_all_valve_close(msg):
+def do_all_valve_close():
     hector.light_on()
     time.sleep(1)
     hector.arm_in()
-
     hector.pump_stop()
     for vnum in range(12):
-        print("Ventil %d wird geschlossen" % (vnum,))
+        log("Ventil %d wird geschlossen" % (vnum,))
         time.sleep(1)
         hector.valve_close(vnum)
     hector.light_off()
 
 
-def do_valve_open(msg):
-    print("open valve")
-    args = str(msg.payload.decode("utf-8")).split(',')
-    if not args[0].isdigit():
-        return
-    index = int(args[0])
-    open = 1
-    if len(args) == 2:
-        if not args[1].isdigit():
-            return
-        open = int(args[1])
+def do_valve_open(index, open):
+    log("open valve")
     hector.valve_open(index, open)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_valve_close(msg):
-    print("close valve")
-    arg = str(msg.payload.decode("utf-8"))
-    if not arg.isdigit():
-        return
-    index = int(arg)
+def do_valve_close(index):
+    log("close valve")
     hector.valve_close(index)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_valve_dose(msg):
-    print("dose valve")
-    args = str(msg.payload.decode("utf-8")).split(',')
-    if not args[0].isdigit():
-        return
-    index = int(args[0])
-    if len(args) < 2:
-        return
-    if not args[1].isdigit():
-        return
-    amount = int(args[1])
-    timeout = 30
-    if len(args) >= 3:
-        if not args[2].isdigit():
-            return
-        timeout = int(args[2])
-    ret = hector.valve_dose(index, amount, timeout, on_callback)
-    client.publish(currentTopic + "/return", str(ret))
+def do_valve_dose(index, amount, timeout=30):
+    log("dose valve")
+    return hector.valve_dose(index, amount, timeout, on_callback)
 
 
-def do_finger(msg):
-    print("set finger position")
-    arg = str(msg.payload.decode("utf-8"))
-    if arg.isdigit():
-        pos = int(arg)
+def do_finger(pos):
+    log("set finger position")
     hector.finger(pos)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def do_ping(msg):
-    args = str(msg.payload.decode("utf-8")).split(',')
-    if not args[0].isdigit():
-        return
-    num = int(args[0])
-    retract = 1
-    if len(args) == 2:
-        if not args[1].isdigit():
-            return
-        retract = int(args[1])
+def do_ping(num, retract):
     hector.ping(num, retract, on_callback)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
 # high-level functions
 
 
-def ring(msg):
-    print("ring the bell")
-    timesToRing = str(msg.payload.decode("utf-8"))
-    if timesToRing.isdigit():
-        hector.ping(int(timesToRing))
-        client.publish(currentTopic + "/return", "ok")
-    else:
-        print("Not a numeric message. Cannot ring the bell.")
+def ring(timesToRing):
+    hector.ping(int(timesToRing))
+    return "ok"
 
 
-def valve_dose(msg):
-    doseArgs = str(msg.payload.decode("utf-8")).split(',')
-    if doseArgs[0].isdigit() and doseArgs[1].isdigit():
-        hector.valve_dose(int(doseArgs[0]), int(doseArgs[1]), cback=on_callback)
-        client.publish(currentTopic + "/return", "ok")
-    else:
-        on_callback("valve_dose", "Not a numeric message. Cant dose liquid.")
-    pass
+def valve_dose(index, amount):
+    hector.valve_dose(index, amount, cback=on_callback)
+    return "ok"
 
 
-def dry(msg):
-    a = str(msg.payload.decode("utf-8"))
-    if a.isdigit():
-        pump = int(a)
-    else:
-        print("Not a numeric message. Cannot dry pump")
-        return
-    print("IndexPump: ", pump)
+def dry(pump):
+    log("IndexPump: " + pump)
     hector.valve_open(pump)
     time.sleep(20)
     hector.valve_open(pump, 0)
-    # on_callback("dry", 1)
-    client.publish(currentTopic + "/return", "ok")
+    return "ok"
 
 
-def clean(msg):
-    a = str(msg.payload.decode("utf-8"))
-    if a.isdigit():
-        pump = int(a)
-    else:
-        on_callback("clean", "Not a numeric message. Cant clean pump")
-        return
-
+def clean(pump):
     hector.pump_start()
     hector.valve_open(pump)
     time.sleep(10)
@@ -258,92 +168,10 @@ def clean(msg):
         hector.valve_open(pump)
         time.sleep(10)
         times += 1
-    print("IndexPump: ", pump)
+    log("IndexPump: ", pump)
     hector.valve_open(pump, 0)
     time.sleep(10)
     hector.pump_stop()
     time.sleep(1)
     # on_callback("clean", 1)
-    client.publish(currentTopic + "/return", "ok")
-
-
-def on_message(client, userdata, msg):
-    try:
-        global currentTopic
-        currentTopic = msg.topic
-
-        if msg.topic.endswith("/progress"):
-            return  # ignore our own progress messages
-        elif msg.topic.endswith("/return"):
-            return  # ignore our own return messages
-
-        # low-level
-        elif msg.topic == TopicPrefix + "get_config":
-            do_get_config(msg)
-        elif msg.topic == TopicPrefix + "light_on":
-            do_light_on(msg)
-        elif msg.topic == TopicPrefix + "light_off":
-            do_light_off(msg)
-        elif msg.topic == TopicPrefix + "arm_out":
-            do_arm_out(msg)
-        elif msg.topic == TopicPrefix + "arm_in":
-            do_arm_in(msg)
-        elif msg.topic == TopicPrefix + "arm_isInOutPos":
-            do_arm_isInOutPos(msg)
-        elif msg.topic == TopicPrefix + "scale_readout":
-            do_scale_readout(msg)
-        elif msg.topic == TopicPrefix + "scale_tare":
-            do_scale_tare(msg)
-        elif msg.topic == TopicPrefix + "pump_start":
-            do_pump_start(msg)
-        elif msg.topic == TopicPrefix + "pump_stop":
-            do_pump_stop(msg)
-        elif msg.topic == TopicPrefix + "valve_open":
-            do_valve_open(msg)
-        elif msg.topic == TopicPrefix + "all_valve_open":
-            do_all_valve_open(msg)
-        elif msg.topic == TopicPrefix + "all_valve_close":
-            do_all_valve_close(msg)
-        elif msg.topic == TopicPrefix + "valve_close":
-            do_valve_close(msg)
-        elif msg.topic == TopicPrefix + "valve_dose":
-            do_valve_dose(msg)
-        elif msg.topic == TopicPrefix + "finger":
-            do_finger(msg)
-        elif msg.topic == TopicPrefix + "ping":
-            do_ping(msg)
-
-        # high-level
-        elif msg.topic == TopicPrefix + "ring":
-            ring(msg)
-        elif msg.topic == TopicPrefix + "doseDrink":
-            valve_dose(msg)
-        elif msg.topic == TopicPrefix + "cleanMe":
-            clean(msg)
-        elif msg.topic == TopicPrefix + "dryMe":
-            dry(msg)
-
-        # unknown
-        else:
-            print("unknown topic: " + msg.topic + ", msg " + str(msg.payload))
-
-        print("done with msg " + msg.topic + " / " + str(msg.payload))
-
-    except Exception as e:
-        print("Error! " + str(e))
-        print(traceback.format_exc())
-
-
-# main program
-if __name__ == "__main__":
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_log = on_log
-
-    client.connect(MQTTServer, 1883, 60)
-
-    while True:
-        client.loop()
-
-# EOF
+    return "ok"
