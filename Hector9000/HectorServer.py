23453a7f1a1b932fe7afa03629df50c082c7cffc
DevTown <marv@dev-town.de>
2021-03-04 20:45:12 +0100

    Default to prodmode

--- Hector9000/HectorServer.py
+++ Hector9000/HectorServer.py
@@ -11,8 +11,8 @@ import re
 import paho.mqtt.client as mqtt
 from Hector9000.conf import HectorConfig as HC
 
-#from Hector9000.HectorHardware import HectorHardware as Hector
-from Hector9000.HectorSimulator import HectorSimulator as Hector
+from Hector9000.HectorHardware import HectorHardware as Hector
+#from Hector9000.HectorSimulator import HectorSimulator as Hector
 
 
 # global vars
